cf86e86bc8070e59148eb40cf7d6f89e1a1c6711
maxs2x <xmaxs2x@gmail.com>
2021-12-15 23:14:24 +0300

    Finished page Colors&Type

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             filename: '[name].[contenthash].css'
         }),
         new HtmlWebpackPlugin({
-            template: "./src/pages/ui-kit/colors-type.pug"
+            template: "./src/pages/ui-kit/colors-type/colors-type.pug"
         })
     ],
     module: {
