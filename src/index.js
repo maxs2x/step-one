a8da779475537b4e3371c0d40dcd101e8cd9e893
maxs2x <xmaxs2x@gmail.com>
2022-01-03 21:11:55 +0300

    ready rooms-search block

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import './styles/index.scss';
 
-import './pages/ui-kit/form-elements/form-elements.js';
+import './pages/ui-kit/cards/cards.js';
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             filename: '[name].[contenthash].css'
         }),
         new HtmlWebpackPlugin({
-            template: "./src/pages/ui-kit/form-elements/form-elements.pug"
+            template: "./src/pages/ui-kit/cards/cards.pug"
         })
     ],
     module: {
