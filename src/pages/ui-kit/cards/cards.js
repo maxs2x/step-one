912696e2b8299619acb5aee94faeaa2eefe0c26d
maxs2x <xmaxs2x@gmail.com>
2022-01-05 19:02:22 +0300

    separated calendar from dropdown-js

--- src/pages/ui-kit/cards/cards.js
+++ src/pages/ui-kit/cards/cards.js
@@ -1,2 +1,10 @@
 import '../../../components/rooms-search/rooms-search.js';
 import '../../../components/reservation/reservation.js';
+import '../../../components/dropdown/dropdown.js';
+import {Calendar, OneDay, RangeDays} from '../../../components/dropdown/_calendar/calendar';
+
+let calendar = document.querySelector('.content-cards__third-column .dropdown-js__month-calendar');
+if (calendar.classList.contains('dropdown-js__inviseble')) {
+    calendar.classList.remove('dropdown-js__inviseble');
+}
+new Calendar(calendar);
