065201f0350b89bf04fe1af9601ad5a6b4774262
maxs2x <xmaxs2x@gmail.com>
2022-01-19 02:38:57 +0300

    building circle chart

--- src/pages/website/room-details/room-details.js
+++ src/pages/website/room-details/room-details.js
@@ -15,21 +15,22 @@ class CircleSegment {
         this.strokeDashoffset = strokeDashoffset;
         this.title = title;
         this.description = description;
-        this.buildingSegment();
+        this.svgPart = this.buildingSegment();
     }
 
     initialisationColor() {
-        let gradient = '<linearGradient id="linearColors' + String(this.numberSegmen) + '" x1="0" y1="0" x2="0" y2="1"> <stop offset="0%" stop-color="' + this.startColor + '"></stop> <stop offset="100%" stop-color="' + this.stopColor + '"></stop>';
+        let gradient = '<linearGradient id="linearColors' + String(this.numberSegmen) + '" x1="0" y1="0" x2="0" y2="1"> <stop offset="0%" stop-color="' + this.startColor + '"></stop> <stop offset="100%" stop-color="' + this.stopColor + '"></stop></linearGradient>';
         return gradient;
     }
 
     initializationCircleSegment() {
-        let circleSegment = '<circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke=url(#linearColors' + this.numberSegmen + ')" stroke-width="1" stroke-dasharray="' + this.startStrokeDasharray + ' ' + this.stopStrokeDasharray + '" stroke-dashoffset="' + this.strokeDashoffset + '" aria-labelledby="donut-segment-' + this.numberSegmen + '-title donut-segment-' + this.numberSegmen + '-desc">';
+        let circleSegment = '<circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="url(#linearColors' + String(this.numberSegmen) + ')" stroke-width="1" stroke-dasharray="' + this.startStrokeDasharray + ' ' + this.stopStrokeDasharray + '" stroke-dashoffset="' + this.strokeDashoffset + '" aria-labelledby="donut-segment-' + this.numberSegmen + '-title donut-segment-' + this.numberSegmen + '-desc"></circle>';
         return circleSegment;
     }
 
     initializationDescription() {
         let description = '<title id="donut-segment-' + String(this.numberSegmen) + '-title">' + this.title + '</title><desc id="donut-segment-' + String(this.numberSegmen) + '-desc">' + this.description + '</desc>'
+        return description;
     }
 
     buildingSegment() {
@@ -47,23 +48,65 @@ class CircleChart {
         this.parent = figure;
         this.dataAboutVoices = dataAboutVoices;
         this.countSegments = this.dataAboutVoices.length;
+        this.filledPart = 0;
+        this.allVoices = this.coutingAllVoices();
+        this.insertAllPartsInSVG();
+        
     }
 
-    calculationStrokeDasharray(countVoices, ) {
-
-    }
-
-    creatingCircleParts() {
-        let oneSegment = '',
-            allSegments = '';
-        for (let segment of this.dataAboutVoices) {
-            let colors = {start: segment.startColor, stop: segment.startColor},
-                strokeDasharray = '?',
-                strokeDashoffset = '?',
-
-            oneSegment = new CircleSegment(segment.segmentNumber, colors, strokeDasharray, strokeDashoffset, segment.title, segment.description);
-            allSegments += oneSegment;
+    coutingAllVoices() {
+        let voices = 0;
+        console.log(this.dataAboutVoices.segmentOne.countVoices);
+        for (let segment of Object.values(this.dataAboutVoices)) {
+            voices += segment.countVoices;
         }
+        return voices;
+    }
+
+    calculationStrokeDasharray(voices, segmentNumber) {
+        let segmentShare = 100 * (voices / this.allVoices);
+        let strokeDasharray = '';
+        if (segmentNumber === 1) {
+            strokeDasharray = {start:String(segmentShare - 2), stop: String(100 - segmentShare + 1)};
+        } else {
+            strokeDasharray = {start:String(segmentShare - 1), stop: String(100 - segmentShare)};
+        }
+        this.filledPart += segmentShare - 1;
+        return strokeDasharray;
+    }
+
+    calculationStrokeDashoffset(segmentNumber) {
+        let strokeDashoffset = 0;
+        if (segmentNumber === 1) {
+            strokeDashoffset = 24;
+        } else {
+            strokeDashoffset = 100 + 25 - this.filledPart - segmentNumber;
+        } 
+        return strokeDashoffset;
+    }
+
+    creatingAllCircleParts() {
+        let oneSegment = '',
+            allSegments = [];
+        for (let segment of Object.values(this.dataAboutVoices)) {
+            let colors = {start: segment.startColor, stop: segment.stopColor},
+                strokeDashoffset = this.calculationStrokeDashoffset(segment.segmentNumber),
+                strokeDasharray = this.calculationStrokeDasharray(segment.countVoices, segment.segmentNumber);
+            console.log(strokeDasharray, strokeDashoffset); 
+            oneSegment = new CircleSegment(segment.segmentNumber, colors, strokeDasharray, strokeDashoffset, segment.title, segment.description);
+            allSegments.push(oneSegment);
+        }
+        return allSegments;
+    }
+
+    insertAllPartsInSVG() {
+        let svgFigure = this.parent.querySelector('svg.donut'),
+            allSegments = this.creatingAllCircleParts(),
+            codeAllSegments = '';
+        for (let codeOneSegment of allSegments) {
+            codeAllSegments += codeOneSegment.svgPart;
+        }
+        svgFigure.insertAdjacentHTML("beforeend", codeAllSegments);
     }
 }
 
