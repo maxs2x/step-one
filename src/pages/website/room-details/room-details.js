e05980704e43e21ab418a649b0019ad2f074b857
maxs2x <xmaxs2x@gmail.com>
2022-01-22 01:11:21 +0300

    ready circle chart

--- src/pages/website/room-details/room-details.js
+++ src/pages/website/room-details/room-details.js
@@ -4,10 +4,20 @@ import '../../../components/header/header.js';
 import '../../../components/reservation/reservation.js';
 import '../../../components/footer/footer.js';
 
+class ChartSegment {
+    constructor(startColor, stopColor, title, description) {
+        this.startColor = startColor;
+        this.stopColor = stopColor;
+        this.title = title;
+        this.description = description;
+    }
+}
+
 class CircleChart {
     constructor(figure, dataAboutVoices) {
         this.parent = figure;
         this.dataAboutVoices = dataAboutVoices;
+        this.segments = new Array();
         this.colors = new Array();
         this.filledPartChart = 0;
         this.allVoices = this.coutingAllVoices();
@@ -45,7 +55,7 @@ class CircleChart {
                 stopSegment = this.calculationStopSegment(startSegment, segmentAngle),
                 spaceBetweenSegments = '#fff ' + (stopSegment) + 'turn ' +  stopSegment + 'turn, ';
             background += startColor + ' ' + startSegment + 'rad ' + stopSegment + 'rad, ' + stopColor + ' ' + (stopSegment - 0.01) + 'turn, ' + spaceBetweenSegments;
-            this.colors.push(startColor);
+            this.segments.push(new ChartSegment(startColor, stopColor, segment.title, segment.description));
         }
         background = 'conic-gradient(' + background.slice(0, -2) + ')';
         console.log('creatingAllCircleParts()');
@@ -53,33 +63,51 @@ class CircleChart {
         return background;
     }
 
+    creatingTextForScreenReader() {
+        let allText = '';  
+        for (let segment of Object.values(this.dataAboutVoices)) {
+            allText += segment.description + ' ' + segment.countVoices + '. ';
+        }
+        return allText;
+    }
+
     creatingLegends() {
         let allTitles = '';  
         for (let segment of Object.values(this.dataAboutVoices)) {
-            let title = '<span class="figure-key__shape-circle shape-' + segment.segmentNumber + '">' + segment.title + '</span>';
-            allTitles += '<li>' + title + '</li>';
+            let title = '<span class="figure-key__shape-circle shape-' + segment.segmentNumber + '"></span>' + segment.title;
+            allTitles += '<li><p>' + title + '</p></li>';
         }
         return allTitles;
     }
 
     creatingStyleForLegends() {
-        let backgroundElement = 'linear-gradient(180deg, #BC9CFF 0%, #8BA4F9 100%);';
+        let styleFigureKeyShapeCircle = `display: inline-block;
+                width: 0.65rem;
+                height: 0.65rem;
+                margin-right: 0.32rem;
+                margin-bottom: 0.18rem;
+                -webkit-border-radius: 50%;
+                -moz-border-radius: 50%;
+                border-radius: 50%;
+                transform: rotate(180deg);
+            `;
         
-        for (let item of this.colors) {
-            let nameElementLegend = '.shape-' + 1,
+        for (let item = 1; item < this.segments.length + 1; item++) {
+            let nameElementLegend = '.shape-' + item,
                 elementLegend = this.parent.querySelector(nameElementLegend);
+                elementLegend.style.cssText = styleFigureKeyShapeCircle + `
+                background: linear-gradient(180deg, ` + this.segments[item-1].startColor + ` 0%, ` + this.segments[item-1].stopColor + ` 100%);
+                `;       
             console.log('creatingStyleForLegends()');
-            console.log(item);
         }
-        this.colors.forEach(alert);
-        console.log(this.colors);
     }
 
     insertHTMLFigure() {
-        let doughnutDiagramm = '  <div class="doughnut-diagramm"><div class="doughnut-diagramm__inner-space"><div class="doughnut-diagramm__inner-text"><p> всего человек</p></div></div></div>',
+        let doughnutDiagramm = '  <div class="doughnut-diagramm"><div class="doughnut-diagramm__inner-space"><div class="doughnut-diagramm__inner-text"><p><span class="doughnut-diagramm__counter">' + String(this.allVoices) + '</span> голосов </p></div></div></div>',
             legendTitles = this.creatingLegends(),
             legend = '<ul class="figure-key__list" aria-hidden="true" role="presentation">' + legendTitles + '</ul>',
-            figcaption = '<figcaption class="figure-key"><p class="screenreader-only"> Круговой график с отзывами. Великолепно 65 отзывов. Хорошо 65 отзывов.</p>' + legend + '</figcaption> ',
+            textForScreenReader = this.creatingTextForScreenReader(),
+            figcaption = '<figcaption class="figure-key"><p class="screenreader-only"> Круговой график. ' + textForScreenReader + '</p>' + legend + '</figcaption> ',
             figure = '<figure>' + doughnutDiagramm + figcaption + '</figure>';    
             console.log('insertHTMLFigure');
         this.parent.insertAdjacentHTML("afterbegin", figure);
@@ -92,49 +120,54 @@ class CircleChart {
                 flex-direction: row;
                 width: min-content;
                 height: min-content;
-                margin: 0.45rem 0rem;
+                margin: 1.45rem 0rem;
             `,
             
             styleDoughnutDiagramm = `position: relative;
-                width: 9.5rem;
-                height: 9.5rem;
+                width: 7.5rem;
+                height: 7.47rem;
+                margin-bottom: 0.2rem;
                 padding: 0rem;
                 border-radius: 50%;
+                transform: scale(-1, 1);
             `,
             
             styleDoughnutDiagrammInnerSpace = `position: absolute;
                 margin: 0px;
                 padding: 0px;
-                top: 5%;
-                left: 5%;
-                width: 90%;
-                height: 90%;
+                top: 3%;
+                left: 3%;
+                width: 94%;
+                height: 94%;
                 border-radius: 50%;
                 background: #fff;
+                display: flex;
+                align-items: center;
+                transform: scale(-1, 1);
+            `,
+            styleDoughnutDiagrammCounterP = `text-align: center;
+            color: #BC9CFF;
+            font-weight: 700;
+            `,
+            styleDoughnutDiagrammCounter = `display: block;
+                font-size: 24px;
             `,
             styleFigureKey = `align-self: flex-end;
-                margin-left: 0.8rem;
-                padding-bottom: 1.0rem;
+                margin-left: 1.73rem;
+                padding-bottom: 0rem;
             `,
             styleFigureKeyList = `margin: 0;
                 padding: 0;
                 list-style: none;
               `,
-              styleFigureKeyListLi = `
+            styleFigureKeyListLi = `
                 display: flex;
                 align-items: center;
-                margin: 0.4rem 0rem 0rem;
+                margin: 0rem 0.1rem 0rem;
                 padding: 0;
+                font-family: 'Montserrat', 'Quicksand', Arial, sans-serif;
+                font-size: 14px;
             `,
-            styleFigureKeyShapeCircle = `display: inline-block;
-                vertical-align: middle;
-                width: 0.65rem;
-                height: 0.65rem;
-                -webkit-border-radius: 50%;
-                -moz-border-radius: 50%;
-                border-radius: 50%;
-            `,
-
             styleScreenreaderOnly = `position: absolute;
                 width: 0.5rem;
                 height: 0.5rem;
@@ -143,53 +176,62 @@ class CircleChart {
                 overflow: hidden;
                 clip: rect(0,0,0,0);
                 border: 0;
+            `,
+            styleDoughnutDiagrammInnerText = `
+                width: 100%;
             `;
 
             this.parent.querySelector('figure').style.cssText = styleContainer;
             this.parent.querySelector('.doughnut-diagramm').style.cssText = styleDoughnutDiagramm;
             this.parent.querySelector('.doughnut-diagramm__inner-space').style.cssText = styleDoughnutDiagrammInnerSpace;
+            this.parent.querySelector('.doughnut-diagramm__inner-text').style.cssText = styleDoughnutDiagrammInnerText;
+            this.parent.querySelector('.doughnut-diagramm__counter').style.cssText = styleDoughnutDiagrammCounter;
+            this.parent.querySelector('.doughnut-diagramm__inner-space p').style.cssText = styleDoughnutDiagrammCounterP;
             this.parent.querySelector('.figure-key').style.cssText = styleFigureKey;
             this.parent.querySelector('.figure-key__list').style.cssText = styleFigureKeyList;
-            this.parent.querySelector('.figure-key__list li').style.cssText = styleFigureKeyListLi;
-            this.parent.querySelector('.figure-key__shape-circle').style.cssText = styleFigureKeyShapeCircle;
+            for (let elem of this.parent.querySelectorAll('.figure-key__list li p')) {
+                elem.style.cssText = styleFigureKeyListLi;
+            }
             this.parent.querySelector('.screenreader-only').style.cssText = styleScreenreaderOnly;
         
-        this.creatingStyleForLegends();
+        
         console.log('insertCSSFigure');
         elementForChart.style.background = this.creatingBackgroundForAllParts();
+        this.creatingStyleForLegends();
 
     }
 }
 
 
-let dataForCircleChart = {segmentOne: {
-        segmentNumber: 1,
-        startColor: '#BC9CFF',
-        stopColor: '#8BA4F9',
-        countVoices: 65,
-        title: 'Удовлетворительно',
-        description: 'Посетители оставившие отзыв "Удовлетворительно"'
-    },
-    segmentTwo: {
-        segmentNumber: 2,
-        startColor: '#6FCF97',
-        stopColor: '#66D2EA',
-        countVoices: 65,
-        title: 'Хорошо',
-        description: 'Посетители оставившие отзыв "Хорошо"'
-    },
+let dataForCircleChart = {
     segmentThree: {
-        segmentNumber: 3,
-        startColor: '#FFBA9C',
-        stopColor: '#FFE39C',
+        segmentNumber: 1,
+        startColor: '#FFE39C',
+        stopColor: '#FFBA9C',
         countVoices: 130,
         title: 'Великолепно',
         description: 'Посетители оставившие отзыв "Великолепно"'
     },
+    segmentTwo: {
+        segmentNumber: 2,
+        startColor: '#66D2EA',
+        stopColor: '#6FCF97',
+        countVoices: 65,
+        title: 'Хорошо',
+        description: 'Посетители оставившие отзыв "Хорошо"'
+    },
+    segmentOne: {
+        segmentNumber: 3,
+        startColor: '#8BA4F9',
+        stopColor: '#BC9CFF',
+        countVoices: 65,
+        title: 'Удовлетворительно',
+        description: 'Посетители оставившие отзыв "Удовлетворительно"'
+    },
     segmentFour: {
         segmentNumber: 4,
-        startColor: '#919191',
-        stopColor: '#3D4975',
+        startColor: '#3D4975',
+        stopColor: '#919191',
         countVoices: 0,
         title: 'Разочарован',
         description: 'Посетители оставившие отзыв "Разочарован"'
