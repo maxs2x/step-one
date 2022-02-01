35e2a75dd4470fc3fcd8a5f81ba0256cc9e647af
maxs2x <xmaxs2x@gmail.com>
2022-02-02 01:29:07 +0300

    corrected grammatical errors

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -20,18 +20,18 @@ var objJson = [
 class Pagination {
     constructor(pagination, pages) {
         this.container = pagination;
-        this.contantPages = pages;
+        this.contentPages = pages;
         this.numberOfPages = pages.length;
         this.prevPage = this.container.querySelector('.js-pagination__button-prev');
         this.thisPage = this.container.querySelector('.js-pagination__hover-page');
         this.nextPage = this.container.querySelector('.js-pagination__button-next');
-        this.lastPage = this.container.querySelector('.js-pagintation__last-page');
-        this.firstPage = this.container.querySelector('.js-pagintation__first-page');
+        this.lastPage = this.container.querySelector('.js-pagination__last-page');
+        this.firstPage = this.container.querySelector('.js-pagination__first-page');
         this.nextPage.onclick = this.handlerNextPage.bind(this);
         this.prevPage.onclick = this.handlerPrevPage.bind(this);
         this.lastPage.onclick = this.handlerLastPage.bind(this);
-        this.firstPage.onclick = this.handlerfirstPage.bind(this);
-        this.thisPage.nextSibling.onclick = this.dubbleNext.bind(this);
+        this.firstPage.onclick = this.handlerFirstPage.bind(this);
+        this.thisPage.nextSibling.onclick = this.doubleNext.bind(this);
         this.alterableContent = this.container.querySelector('.js-pagination__count-from-to');
         this.setLastPage();
         this.bindingEvent();
@@ -41,7 +41,7 @@ class Pagination {
         this.lastPage.innerHTML = this.numberOfPages;
         if (this.numberOfPages < 4) {
             let nextFreePoint = this.lastPage.previousSibling;
-            nextFreePoint.classList.add('js-pagination__invis-element');
+            nextFreePoint.classList.add('js-pagination__invisible-element');
         }
     }
 
@@ -52,13 +52,13 @@ class Pagination {
             let nextNumberPage = Number(this.thisPage.innerHTML) + 1;
             if ((nextNumberPage + 1) === this.numberOfPages) {
                 console.log(this.thisPage.nextSibling);
-                this.thisPage.nextSibling.classList.add('js-pagination__invis-element');
+                this.thisPage.nextSibling.classList.add('js-pagination__invisible-element');
             }
             if (nextNumberPage === this.numberOfPages) {
-                this.lastPage.classList.add('js-pagination__invis-element');
+                this.lastPage.classList.add('js-pagination__invisible-element');
             }
             if (nextNumberPage > this.numberOfPages) {
-                this.nextPage.classList.add('js-pagination__invis-element');
+                this.nextPage.classList.add('js-pagination__invisible-element');
                 return
             }
             buttonForMoving.innerHTML = nextNumberPage;
@@ -67,13 +67,13 @@ class Pagination {
             let buttonForMoving = this.container.querySelectorAll('.js-pagination__page')[2];
             let nextNumberPage = Number(this.thisPage.innerHTML) - 1;
             if ((nextNumberPage - 1) === 1) {
-                this.thisPage.previousSibling.classList.add('js-pagination__invis-element');
+                this.thisPage.previousSibling.classList.add('js-pagination__invisible-element');
             }
             if (nextNumberPage === 1) {
-                this.firstPage.classList.add('js-pagination__invis-element');
+                this.firstPage.classList.add('js-pagination__invisible-element');
             }
             if (nextNumberPage === 0) {
-                this.prevPage.classList.add('js-pagination__invis-element');
+                this.prevPage.classList.add('js-pagination__invisible-element');
                 return
             }
             buttonForMoving.innerHTML = nextNumberPage;
@@ -82,37 +82,37 @@ class Pagination {
 
     }
 
-    recordingCahnges(contentPage) {
+    recordingChanges(contentPage) {
         this.alterableContent.innerHTML = contentPage.adName;
     }
 
     updatePrevButtons(numberPage) {
         switch (numberPage) {
             case 2:
-                this.prevPage.classList.remove('js-pagination__invis-element');
+                this.prevPage.classList.remove('js-pagination__invisible-element');
                 break;
             case 3:
-                this.firstPage.classList.remove('js-pagination__invis-element');
+                this.firstPage.classList.remove('js-pagination__invisible-element');
                 break;
             case 4:
-                this.container.querySelectorAll('.js-pagination__three-point')[0].classList.remove('js-pagination__invis-element');
+                this.container.querySelectorAll('.js-pagination__three-point')[0].classList.remove('js-pagination__invisible-element');
                 break;
         }
     }
 
-    updateNexttons(numberPage) {
+    updateNextButtons(numberPage) {
         switch (numberPage) {
             case (this.numberOfPages - 1):
                 // показать кнопку следующая страница
-                this.nextPage.classList.remove('js-pagination__invis-element');
+                this.nextPage.classList.remove('js-pagination__invisible-element');
                 break;
             case (this.numberOfPages - 2):
                 // показать кнопку последняя страница
-                this.lastPage.classList.remove('js-pagination__invis-element');
+                this.lastPage.classList.remove('js-pagination__invisible-element');
                 break;
             case (this.numberOfPages - 3):
                 // показать три точки спереди
-                this.container.querySelectorAll('.js-pagination__three-point')[1].classList.remove('js-pagination__invis-element');
+                this.container.querySelectorAll('.js-pagination__three-point')[1].classList.remove('js-pagination__invisible-element');
                 break;
         }
     }
@@ -143,23 +143,23 @@ class Pagination {
         allPaginationPage[1].innerHTML = (this.numberOfPages - 2);
         let allTreePoint = this.container.querySelectorAll('.js-pagination__three-point');
         allTreePoint[0].insertAdjacentElement('afterend', allPaginationPage[1]);
-        allTreePoint[1].classList.add('js-pagination__invis-element');
-        let lastPage = '<div class="pagintation__last-page js-pagintation__last-page  js-pagination__invis-element">15</div>';
+        allTreePoint[1].classList.add('js-pagination__invisible-element');
+        let lastPage = '<div class="pagination__last-page js-pagination__last-page  js-pagination__invisible-element">15</div>';
         this.thisPage.insertAdjacentHTML('afterend', lastPage);
         this.thisPage.insertAdjacentElement('afterend', allTreePoint[1]);
-        this.lastPage = this.container.querySelector('.js-pagintation__last-page');
+        this.lastPage = this.container.querySelector('.js-pagination__last-page');
         this.lastPage.onclick = this.handlerLastPage.bind(this);
         // показываем кнопку назад если ее нет
-        if (this.prevPage.classList.contains('js-pagination__invis-element')) {
-            this.prevPage.classList.remove('js-pagination__invis-element');
+        if (this.prevPage.classList.contains('js-pagination__invisible-element')) {
+            this.prevPage.classList.remove('js-pagination__invisible-element');
         }
         // показываем первую страницу если ее нет
-        if (this.firstPage.classList.contains('js-pagination__invis-element')) {
-            this.firstPage.classList.remove('js-pagination__invis-element');
+        if (this.firstPage.classList.contains('js-pagination__invisible-element')) {
+            this.firstPage.classList.remove('js-pagination__invisible-element');
         }
         // показываем ... назад если их нет
-        if (this.firstPage.nextSibling.classList.contains('js-pagination__invis-element')) {
-            this.firstPage.nextSibling.classList.remove('js-pagination__invis-element');
+        if (this.firstPage.nextSibling.classList.contains('js-pagination__invisible-element')) {
+            this.firstPage.nextSibling.classList.remove('js-pagination__invisible-element');
         }
     }
 
@@ -176,29 +176,29 @@ class Pagination {
         paginationPages[0].classList.add('js-pagination__hover-page');
         this.thisPage = paginationPages[0];
         // показываем кнопку вперед если ее нет
-        if (this.nextPage.classList.contains('js-pagination__invis-element')) {
-            this.nextPage.classList.remove('js-pagination__invis-element');
+        if (this.nextPage.classList.contains('js-pagination__invisible-element')) {
+            this.nextPage.classList.remove('js-pagination__invisible-element');
         }
         // показываем последнюю страницу если ее нет
-        if (this.lastPage.classList.contains('js-pagination__invis-element')) {
-            this.lastPage.classList.remove('js-pagination__invis-element');
+        if (this.lastPage.classList.contains('js-pagination__invisible-element')) {
+            this.lastPage.classList.remove('js-pagination__invisible-element');
         }
         // показываем ... вперед если их нет
-        if (this.lastPage.previousSibling.classList.contains('js-pagination__invis-element')) {
-            this.lastPage.previousSibling.classList.remove('js-pagination__invis-element');
+        if (this.lastPage.previousSibling.classList.contains('js-pagination__invisible-element')) {
+            this.lastPage.previousSibling.classList.remove('js-pagination__invisible-element');
         }
     }
 
     handlerPrevPage() {
         let thisPageNumber = Number(this.thisPage.innerHTML);
-        let nextPageContent = this.contantPages[thisPageNumber - 2];
-        this.recordingCahnges(nextPageContent);
+        let nextPageContent = this.contentPages[thisPageNumber - 2];
+        this.recordingChanges(nextPageContent);
         this.movingHover(thisPageNumber - 1);
-        this.updateNexttons(thisPageNumber - 1);
+        this.updateNextButtons(thisPageNumber - 1);
         this.bindingEvent();
     }
 
-    dubblePrev() {
+    doublePrev() {
         this.handlerPrevPage();
         this.handlerPrevPage();
         this.bindingEvent();
@@ -206,14 +206,14 @@ class Pagination {
 
     handlerNextPage() {
         let thisPageNumber = Number(this.thisPage.innerHTML);
-        let nextPageContent = this.contantPages[thisPageNumber];
-        this.recordingCahnges(nextPageContent);
+        let nextPageContent = this.contentPages[thisPageNumber];
+        this.recordingChanges(nextPageContent);
         this.movingHover(thisPageNumber + 1);
         this.updatePrevButtons(thisPageNumber + 1);
         this.bindingEvent();
     }
 
-    dubbleNext() {
+    doubleNext() {
         this.handlerNextPage();
         this.handlerNextPage();
         this.bindingEvent();
@@ -221,10 +221,10 @@ class Pagination {
 
     handlerLastPage() {
         let thisPageNumber = this.numberOfPages;
-        let nextPageContent = this.contantPages[thisPageNumber - 1];
-        this.recordingCahnges(nextPageContent);
+        let nextPageContent = this.contentPages[thisPageNumber - 1];
+        this.recordingChanges(nextPageContent);
         let newLastPage = this.lastPage;
-        newLastPage.classList.remove('js-pagintation__last-page');
+        newLastPage.classList.remove('js-pagination__last-page');
         newLastPage.classList.add('js-pagination__page');
         this.movingHover(this.numberOfPages);
         this.updatePagination('next');
@@ -232,11 +232,11 @@ class Pagination {
         this.bindingEvent();
     }
 
-    handlerfirstPage() {
-        this.recordingCahnges(this.contantPages[0]);
-        this.firstPage.classList.add('js-pagination__invis-element');
-        this.prevPage.classList.add('js-pagination__invis-element');
-        this.container.querySelector('.js-pagination__three-point').classList.add('js-pagination__invis-element');
+    handlerFirstPage() {
+        this.recordingChanges(this.contentPages[0]);
+        this.firstPage.classList.add('js-pagination__invisible-element');
+        this.prevPage.classList.add('js-pagination__invisible-element');
+        this.container.querySelector('.js-pagination__three-point').classList.add('js-pagination__invisible-element');
         this.updatePaginationForFirstPage();
         this.bindingEvent();
 
@@ -249,11 +249,11 @@ class Pagination {
             allElementsPage[2].onclick = this.handlerNextPage.bind(this);
             allElementsPage[1].onclick = () => ({chill:'chill'});
         } else if (allElementsPage[0].classList.contains('js-pagination__hover-page')) {
-            allElementsPage[2].onclick = this.dubbleNext.bind(this);
+            allElementsPage[2].onclick = this.doubleNext.bind(this);
             allElementsPage[1].onclick = this.handlerNextPage.bind(this);
             allElementsPage[0].onclick = () => ({chill:'chill'});
         } else if (allElementsPage[2].classList.contains('js-pagination__hover-page')) {
-            allElementsPage[0].onclick = this.dubblePrev.bind(this);
+            allElementsPage[0].onclick = this.doublePrev.bind(this);
             allElementsPage[1].onclick = this.handlerPrevPage.bind(this);
             allElementsPage[2].onclick = () => ({chill:'chill'});
         }
