0d226ca04986632d74fc866c7b5bcce1908546b0
maxs2x <xmaxs2x@gmail.com>
2022-02-07 21:07:19 +0300

    optimizer all image

--- src/pages/website/landing-page/landing-page.js
+++ src/pages/website/landing-page/landing-page.js
@@ -1,5 +1,6 @@
 import './landing-page.scss';
 
+import '../../../favicon/favicon';
 import '../../../components/header/header.js';
 import '../../../components/rooms-search/rooms-search.js';
 import '../../../components/footer/footer.js';
